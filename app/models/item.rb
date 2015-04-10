fdcf396ac4bb4916747acecd4bf30ef04d83adde
Harley Trung <harley@socialsci.com>
2015-04-10 08:12:48 +0700

    add searchable blokc under Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ActiveRecord::Base
   extend Enumerize
   enumerize :kind, in: [:book], default: :book
+
+  searchable do
+    text :title
+    text :kind
+    text :author
+    text :summary
+    integer :number_of_pages
+    text :code
+    text :publisher
+  end
 end

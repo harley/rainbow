5470a292c694ed1f24c3b04bb07f9cd8b58c1eb6
Harley Trung <harley@tinypulse.com>
2018-03-31 12:10:45 +0700

    update rails to fix compatibility with ruby 2.4

--- config/initializers/rails_admin_import.rb
+++ config/initializers/rails_admin_import.rb
@@ -1,28 +1,29 @@
-RailsAdminImport.config do |config|
-  config.model Item do
-    excluded_fields do
-      [:filepicker_url]
-    end
-    # label :title
-    # extra_fields do
-    #   [:field3, :field4, :field5]
-    # end
-  end
-
-  config.model User do
-    excluded_fields do
-      [
-       :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at,
-       :sign_in_count, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_at, :last_sign_in_ip,
-       :added_item
-      ]
-    end
-  end
-
-  config.model Member do
-    excluded_fields do
-      []
-    end
-    label :sid
-  end
-end
+# TODO: fix exclude_fields: wrong number of arguments
+# RailsAdminImport.config do |config|
+#   config.model Item do
+#     excluded_fields do
+#       [:filepicker_url]
+#     end
+#     # label :title
+#     # extra_fields do
+#     #   [:field3, :field4, :field5]
+#     # end
+#   end
+#
+#   config.model User do
+#     excluded_fields do
+#       [
+#        :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at,
+#        :sign_in_count, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_at, :last_sign_in_ip,
+#        :added_item
+#       ]
+#     end
+#   end
+#
+#   config.model Member do
+#     excluded_fields do
+#       []
+#     end
+#     label :sid
+#   end
+# end
